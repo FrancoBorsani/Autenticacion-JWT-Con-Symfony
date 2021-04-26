6e8bfd65bc7e151e20e7c59f4d1c0e09f541dfd9
FrancoBorsani <borsafranco@gmail.com>
2021-04-26 16:47:00 -0300

    PHPUnit

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,8 +8,20 @@ use Symfony\Component\HttpFoundation\Response;
 use App\Entity\User;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use PHPUnit\Framework\TestCase;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
 class DefaultController extends AbstractController
 {
+    public function a()
+    {
+        return $this->render('base.html.twig');
+    }
+
+
     public function register(Request $request, UserPasswordEncoderInterface $encoder)
     {
         $em = $this->getDoctrine()->getManager();
